ac6b5d6d0a1a315fa1b2ee08306058d49599d8a6
khankit <khankit5595@gmail.com>
2023-05-31 06:50:20 +0000

    Added all project files in first commit

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,19 @@
+package com.mycompany.app;
+
+/**
+ * Hello world!
+ */
+public class App {
+
+    private static final String MESSAGE = "I hate Jenkins";
+
+    public App() {}
+
+    public static void main(String[] args) {
+        System.out.println("Hello there ! I am complex Java app");
+    }
+
+    public String getMessage() {
+        return MESSAGE;
+    }
+}
