326c6098e76b63dabefd375e53dd08bbbccc840a
khankit <khankit5595@gmail.com>
2023-05-31 07:33:10 +0000

    khankit first change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Hello there ! I am complex Java app");
+        System.out.println("wow, today weather is so colddd!");
     }
 
     public String getMessage() {
