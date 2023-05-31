19db0f64368e167d8cffc92e2d728e7621a49725
khankit <khankit5595@gmail.com>
2023-05-31 08:54:50 +0000

    khankit third change in the local project folder

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -10,7 +10,7 @@ public class App {
     public App() {}
 
     public static void main(String[] args) {
-        System.out.println("Lets go badminton after jenkins training !");
+        System.out.println("Lets go badminton after jenkins training... I might late due to training end late. !");
     }
 
     public String getMessage() {
